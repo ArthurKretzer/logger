85b5a52551b29e02d28a0601ececa67bd2d7d94f
ArthurKretzer <arthur.raulino.kretzer@gmail.com>
2023-07-14 11:24:20 -0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,8 @@
+from logger import log
+
+logger = log("example")
+
+logger.info("This is an information.")
+logger.warning("This is a warning.")
+logger.error("This is an error.")
+logger.debug("This is a debug.")
